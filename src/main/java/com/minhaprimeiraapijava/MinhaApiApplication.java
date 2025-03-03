338cbd15083292057cd28474982b327a000dfb73
Matheus Silveira <matheus.silveiraw@gmail.com>
2025-03-03 17:38:46 -0300

    primeiro teste com api

---
+++ src/main/java/com/minhaprimeiraapijava/MinhaApiApplication.java
@@ -0,0 +1,12 @@
+package com.minhaprimeiraapijava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhaApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/minhaprimeiraapijava/minha_api/MinhaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minhaprimeiraapijava.minha_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
