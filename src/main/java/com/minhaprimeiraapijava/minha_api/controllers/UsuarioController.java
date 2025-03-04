cf5a730475339df3a780716ac4c276228923498e
Matheus Silveira <matheus.silveiraw@gmail.com>
2025-03-04 17:55:31 -0300

    criando resposta no controller

--- src/main/java/com/minhaprimeiraapijava/minha_api/controllers/UsuarioController.java
+++ src/main/java/com/minhaprimeiraapijava/minha_api/controllers/UsuarioController.java
@@ -3,10 +3,15 @@ package com.minhaprimeiraapijava.minha_api.controllers;
 import com.minhaprimeiraapijava.minha_api.models.Usuario;
 import com.minhaprimeiraapijava.minha_api.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/usuarios")
@@ -15,9 +20,25 @@ public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
 
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
     @PostMapping
-    public Usuario criarUsuario(@RequestBody Usuario usuario) {
-        return usuarioService.criarUsuario(usuario);
+    public ResponseEntity<Map<String, Object>> criarUsuario(@RequestBody Usuario usuario) {
+        Map<String, Object> response = new HashMap<>();
+        try {
+            usuarioService.criarUsuario(usuario);
+            // Prepara a resposta em formato JSON
+            response.put("status", HttpStatus.CREATED.value()); // 201
+            response.put("message", "Usuário criado com sucesso");
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        } catch (Exception e) {
+            // Prepara a resposta de erro em formato JSON
+            response.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value()); // 500
+            response.put("message", "Erro ao criar o usuário: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        }
     }
 
     @GetMapping
