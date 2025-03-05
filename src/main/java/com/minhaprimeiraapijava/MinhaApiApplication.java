3c971800e93d4985941d9af41c03e62db121fb70
Matheus Silveira <matheus.silveiraw@gmail.com>
2025-03-05 08:25:05 -0300

    anotações arquivo inicial

--- src/main/java/com/minhaprimeiraapijava/MinhaApiApplication.java
+++ src/main/java/com/minhaprimeiraapijava/MinhaApiApplication.java
@@ -3,10 +3,10 @@ package com.minhaprimeiraapijava;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication //define o ponto de entrada da aplicação
 public class MinhaApiApplication {
 
-	public static void main(String[] args) { //no java isso que define onde a aplicação vai rodar
-		SpringApplication.run(MinhaApiApplication.class, args);
+	public static void main(String[] args) { //é o ponto de entrada de um programa Java
+		SpringApplication.run(MinhaApiApplication.class, args); //inicia o spring boot
 	}
 }
